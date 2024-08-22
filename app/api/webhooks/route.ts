5cf6a8eacfdadea5c53b4fe065e5b051adc1d002
Louiskhen Yagdulas <yagdulasl@gmail.com>
2024-08-22 14:46:30 +0800

    fixing resend api

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -72,7 +72,7 @@ export async function POST(req: Request) {
       });
 
       await resend.emails.send({
-        from: "InkedShell <yagdulasl@gmail.com>",
+        from: "InkedShell <onboarding@resend.dev>",
         to: [event.data.object.customer_details.email],
         subject: "Thanks for your order!",
         react: OrderReceivedEmail({
